6bf65fa62ba52c12c1130586165f3481dace50db
sjagne <shashankjgn@gmail.com>
2025-06-25 18:09:19 +0530

    First commit after making django form

---
+++ myapp/urls.py
@@ -0,0 +1,11 @@
+from myapp import views
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+]
+
+
+
+
---
+++ myapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages  # for showing error messages
+
+
+
+def login_view(request):
+
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password.')
+
+    return render(request, 'login.html')  # GET request or failed login
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
